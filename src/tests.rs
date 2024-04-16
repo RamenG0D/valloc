61b8ad78a17abeb12b4b84ad5a63046e0975e1ea
drplague1 <justin.bradshaw1203@gmail.com>
2024-04-16 10:27:10 -0400

    replace build file with feature based test

--- src/tests.rs
+++ src/tests.rs
@@ -288,3 +288,25 @@ fn many_ptr_stress_test() {
         free(&mut a, ptr).unwrap();
     }
 }
+
+#[cfg(feature = "C")]
+#[test]
+fn build_header() {
+    println!("Checking for `cbindgen`...");
+    if let Err(std::io::ErrorKind::NotFound) = Command::new("cbindgen").arg("--version").spawn().map_err(|e| e.kind()) {
+        eprintln!("Failed to Find `cbindgen` is it installed?");
+        println!("It can be installed with \"cargo install cbindgen\"");
+        std::process::exit(1);
+    } else {
+        println!("`cbindgen` Found!");
+    }
+
+    println!("Generating Bindings...");
+    Command::new("cbindgen").args([
+        "-o", "valloc.h", 
+        "--config", "cbindings.toml", 
+        "--crate", "valloc", 
+        "--lang", "c"
+    ]).spawn().unwrap().wait().expect("Failed to generate bindings");
+    println!("Binding Generation Succesful!");
+}
